51df07289af4ef25e32b0b7d61499f221b1d71e5
Gustavo Gois <gustavo.menezes.gois@cgd.pt>
2017-08-14 11:12:50 +0100

    Step 02 (complete - with empty list alternative - ResponseEntity)

--- algamoney-api/src/main/java/pt/gois/algaworks/algamoneyapi/resource/CategoriaResource.java
+++ algamoney-api/src/main/java/pt/gois/algaworks/algamoneyapi/resource/CategoriaResource.java
@@ -1,6 +1,7 @@
 package pt.gois.algaworks.algamoneyapi.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,22 @@ public class CategoriaResource {
 
     @GetMapping
     public List<Categoria> listar() {
+
         return categoriaRepository.findAll();
     }
+
+
+//    // Instructor suggests previous approach to returning an empty list
+//    @GetMapping
+//    public ResponseEntity<?> listar() {
+//
+//        List<Categoria> categorias = categoriaRepository.findAll();
+//        // 404 - Not Found. Not the most suitable, would indicate that didn't find the resource, URL has changed,
+//        // wich is not the case...
+//        // return categorias.isEmpty() ? ResponseEntity.notFound().build() : ResponseEntity.ok(categorias);
+//        // 204 - starts with '2', what means that the response is fine. But, 204 indicates that it´s fine, but,
+//        // there isn´t nothing to show.
+//        return categorias.isEmpty() ? ResponseEntity.noContent().build() : ResponseEntity.ok(categorias);
+//    }
 
 }
