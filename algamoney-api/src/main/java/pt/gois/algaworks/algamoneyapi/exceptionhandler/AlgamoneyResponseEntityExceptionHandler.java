cc764c839c3e1d4964a200c4bdaddc45e00f8d98
Gustavo Gois <gustavo.menezes.gois@cgd.pt>
2017-08-18 17:34:22 +0100

    Step 10 (complete - with user and developers messages)

--- algamoney-api/src/main/java/pt/gois/algaworks/algamoneyapi/exceptionhandler/AlgamoneyResponseEntityExceptionHandler.java
+++ algamoney-api/src/main/java/pt/gois/algaworks/algamoneyapi/exceptionhandler/AlgamoneyResponseEntityExceptionHandler.java
@@ -64,9 +64,14 @@ public class AlgamoneyResponseEntityExceptionHandler extends ResponseEntityExcep
     }
 
     @ExceptionHandler({EmptyResultDataAccessException.class})
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    public void handleEmptyResultDataAccessException(RuntimeException e) {
+    public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException ex,
+                                                                       WebRequest request) {
 
+        String mensagemUsuario = messageSource.getMessage("recurso.nao-encontrado", null, LocaleContextHolder.getLocale());
+        String mensagemDesenvolvedor = ex.toString();
+        List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+
+        return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
     }
 
     public static class Erro {
