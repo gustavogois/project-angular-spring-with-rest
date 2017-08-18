4a6c571703536861a01798273e3a4062ceafb4e5
Gustavo Gois <gustavo.gois@gmail.com>
2017-08-19 00:30:50 +0100

    Step 12.1

--- algamoney-api/src/main/resources/db/migration/V003__ajustar_criar_pessoas.sql
+++ algamoney-api/src/main/resources/db/migration/V003__ajustar_criar_pessoas.sql
@@ -1,20 +1,29 @@
-DROP TABLE IF EXISTS pessoa;
+CREATE TABLE lancamento (
+	codigo BIGINT(20) PRIMARY KEY AUTO_INCREMENT,
+	descricao VARCHAR(50) NOT NULL,
+	data_vencimento DATE NOT NULL,
+	data_pagamento DATE,
+	valor DECIMAL(10,2) NOT NULL,
+	observacao VARCHAR(100),
+	tipo VARCHAR(20) NOT NULL,
+	codigo_categoria BIGINT(20) NOT NULL,
+	codigo_pessoa BIGINT(20) NOT NULL,
+	FOREIGN KEY (codigo_categoria) REFERENCES categoria(codigo),
+	FOREIGN KEY (codigo_pessoa) REFERENCES pessoa(codigo)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE pessoa (
-  codigo            BIGINT(20)  PRIMARY KEY AUTO_INCREMENT,
-  nome              VARCHAR(50) NOT NULL,
-  ativo             TINYINT(1)  NULL DEFAULT 0,
-  logradouro     VARCHAR(60) NULL,
-  numero         VARCHAR(5)  NULL,
-  complemento    VARCHAR(60) NULL,
-  bairro         VARCHAR(30) NULL,
-  cep            VARCHAR(9)  NULL,
-  cidade         VARCHAR(30) NULL,
-  estado         VARCHAR(30) NULL
-) ENGINE = InnoDB DEFAULT CHARSET=utf8;
-
-insert into pessoa (ativo, nome, logradouro, numero, complemento, bairro, cep, cidade, estado)
-			values (1, 'Gustavo Gois', 'Praceta das Orquideas', 1, '', 'Odivelas', '2675-578', 'Lisboa', 'Lisboa');
-
-insert into pessoa (ativo, nome, logradouro, numero, complemento, bairro, cep, cidade, estado)
-			values (1, 'Joao da Silva', 'Edf Piramide', 1, '', 'Águas Claras', '32100-280', 'Brasilia', 'DF');
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) values ('Salário mensal', '2017-06-10', null, 6500.00, 'Distribuição de lucros', 'RECEITA', 1, 1);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) values ('Bahamas', '2017-02-10', '2017-02-10', 100.32, null, 'DESPESA', 2, 2);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) values ('Top Club', '2017-06-10', null, 120, null, 'RECEITA', 3, 3);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) values ('CEMIG', '2017-02-10', '2017-02-10', 110.44, 'Geração', 'RECEITA', 3, 4);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) values ('DMAE', '2017-06-10', null, 200.30, null, 'DESPESA', 3, 5);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) values ('Extra', '2017-03-10', '2017-03-10', 1010.32, null, 'RECEITA', 4, 6);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) values ('Bahamas', '2017-06-10', null, 500, null, 'RECEITA', 1, 7);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) values ('Top Club', '2017-03-10', '2017-03-10', 400.32, null, 'DESPESA', 4, 8);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) values ('Despachante', '2017-06-10', null, 123.64, 'Multas', 'DESPESA', 3, 9);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) values ('Pneus', '2017-04-10', '2017-04-10', 665.33, null, 'RECEITA', 5, 10);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) values ('Café', '2017-06-10', null, 8.32, null, 'DESPESA', 1, 5);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) values ('Eletrônicos', '2017-04-10', '2017-04-10', 2100.32, null, 'DESPESA', 5, 4);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) values ('Instrumentos', '2017-06-10', null, 1040.32, null, 'DESPESA', 4, 3);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) values ('Café', '2017-04-10', '2017-04-10', 4.32, null, 'DESPESA', 4, 2);
+INSERT INTO lancamento (descricao, data_vencimento, data_pagamento, valor, observacao, tipo, codigo_categoria, codigo_pessoa) values ('Lanche', '2017-06-10', null, 10.20, null, 'DESPESA', 4, 1);
