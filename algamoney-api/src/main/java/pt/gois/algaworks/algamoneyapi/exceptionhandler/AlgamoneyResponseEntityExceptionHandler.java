0b26ecc91dd1c08f254ac8e27e155db4c1fae182
Gustavo Gois <gustavo.gois@gmail.com>
2017-08-20 17:29:50 +0100

    step 13.2

--- algamoney-api/src/main/java/pt/gois/algaworks/algamoneyapi/exceptionhandler/AlgamoneyResponseEntityExceptionHandler.java
+++ algamoney-api/src/main/java/pt/gois/algaworks/algamoneyapi/exceptionhandler/AlgamoneyResponseEntityExceptionHandler.java
@@ -1,8 +1,10 @@
 package pt.gois.algaworks.algamoneyapi.exceptionhandler;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -72,6 +74,17 @@ public class AlgamoneyResponseEntityExceptionHandler extends ResponseEntityExcep
         List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
 
         return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+
+    @ExceptionHandler({DataIntegrityViolationException.class})
+    public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException ex,
+                                                                        WebRequest request) {
+        String mensagemUsuario = messageSource.getMessage("recurso.operacao-nao-permitido", null, LocaleContextHolder.getLocale());
+        String mensagemDesenvolvedor = ExceptionUtils.getRootCauseMessage(ex);
+        List<Erro> erros = Arrays.asList(new Erro(mensagemUsuario, mensagemDesenvolvedor));
+
+        return handleExceptionInternal(ex, erros, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+
     }
 
     public static class Erro {
