1c5da0b38ea2bcc5f72db803cc40d686509270d8
Gustavo Gois <gustavo.gois@gmail.com>
2017-08-21 23:10:03 +0100

    Step 18

---
+++ algamoney-api/src/main/java/pt/gois/algaworks/algamoneyapi/config/SecurityConfig.java
@@ -0,0 +1,39 @@
+package pt.gois.algaworks.algamoneyapi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+/**
+ * Created by Gustavo on 21/08/2017.
+ */
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ROLE");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // /categorias qualquer um pode acessar
+        // o resto precisa estar autenticado
+        // desabilitar criação de sessão no servidor
+        // desabilitar csrf - javascript injection
+        http.authorizeRequests().antMatchers("/categorias").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .csrf().disable();
+
+    }
+}
